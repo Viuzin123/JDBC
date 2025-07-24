2bdd647dd0ee54cf44e1e70154b6eb43026d66fa
Vitor Campos <vitinhoc2005@gmail.com>
2025-07-24 16:35:52 -0300

    Class Department created

---
+++ src/Application/Main.java
@@ -0,0 +1,16 @@
+package Application;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Department dp = new Department(1, "Books");
+		
+	
+		System.out.println(dp);
+
+	}
+
+}
