040a9695039215212d8b8e3224a162bd79f9caf3
Vitor Campos <vitinhoc2005@gmail.com>
2025-07-24 17:03:55 -0300

    Class Seller created

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,15 +1,20 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
 		Department dp = new Department(1, "Books");
+		Seller s = new Seller(1, "Vitor", "vitaovitin@hotmail.com", new Date(), 30000.00, dp);
 		
 	
 		System.out.println(dp);
+		System.out.println(s);
 
 	}
 
